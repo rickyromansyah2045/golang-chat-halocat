95a117fc3a6b4e05a4a31f89d998e6b70bb65d87
RomansyahOfficial <ricky@hotelmurah.com>
2023-06-12 17:50:52 +0700

    first commit

---
+++ router/router.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"server/internal/user"
+	"server/internal/ws"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
+	r = gin.Default()
+	r.SetTrustedProxies(nil)
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Host", "Origin", "Content-Length", "Content-Type", "Authorization", "User-Agent", "X-Forwarded-For", "Accept-Encoding", "Connection"},
+		ExposeHeaders:    []string{"Content-Length", "Content-Encoding"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.GET("/", userHandler.HelloWorld)
+	r.POST("/signup", userHandler.CreateUser)
+	r.POST("/login", userHandler.Login)
+	r.GET("/logout", userHandler.Logout)
+
+	r.POST("/ws/createRoom", wsHandler.CreateRoom)
+	r.GET("/ws/joinRoom/:roomId", wsHandler.JoinRoom)
+	r.GET("/ws/getRooms", wsHandler.GetRooms)
+	r.GET("/ws/getClients/:roomId", wsHandler.GetClients)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
